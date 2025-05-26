ef922439323e1ac9dfc33c7e2a4060ea838544ec
paulo <paulo200825@gmail.com>
2025-05-26 17:16:48 -0300

    Aula4

---
+++ Aulas/Aula4/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Application } from "express";
+import userRoutes from "./routes/UserRoutes";
+import { AppDataSource } from "./database/data-source";
+
+const app: Application = express();
+
+app.use(express.json());
+AppDataSource.initialize()
+.then(() => {
+
+    app.use('/api', userRoutes);
+
+    app.listen(3000, () => console.log('Server rodando na porta 3000'));
+})
+.catch((error) => console.log(error));
+
