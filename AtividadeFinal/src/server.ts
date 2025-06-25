f614ebb36a4d5f78e15fc67e4af8b8dfeaf5b89d
paulo <paulo200825@gmail.com>
2025-06-25 17:15:37 -0300

    Atividade Avaliativa 3.0

--- AtividadeFinal/src/server.ts
+++ AtividadeFinal/src/server.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 import path from "path";
 
 const app: Application = express();
-    
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
